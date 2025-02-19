735860885f328938dd2c1b5667914df260a5bbae
Pulasky <pulasky01@outlook.es>
2025-02-19 19:32:10 +0100

    Remove indexgpt.html, estilos.css, and script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const descripciones = ["Colina en un lago", "Campo de girasoles", 
-    "Anochecer en la costa", "Arco de roca", "Campo de flores", 
-    "Amanecer en el mar"];   
-    function mostrarinfo(index) {
-        const infoDiv= document.getElementById("info");
-        infoDiv.innerHTML="<p>" + descripciones[index] + "</p>";
-        infoDiv.classList.remove("hidden");}
