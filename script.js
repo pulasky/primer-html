f2007777c5ea445c7c6392e7a0afeeb91e13547f
Pulasky <pulasky01@outlook.es>
2025-02-20 19:13:30 +0100

    Commit modified files

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const descripciones= [
-  "Colina en un lago",
-  "Campo de girasoles",
-  "Puesta de sol en la costa",
-  "Arco de roca",
-  "Campo de flores",
-  "Amanecer en el mar"];
-  function mostrarInfo(index) {   const infoDiv = document.getElementById("info");
-    infoDiv.innerHTML = "<p>" + descripciones[index] + "</p>";
-infoDiv.classList.remove("hidden");
-  }
+ const descripciones= [
+    "Colina en un lago",
+    "Campo de girasoles",
+    "Puesta de sol en la costa",
+    "Arco de roca",
+    "Campo de flores",
+    "Amanecer en el mar"];
+    function mostrarInfo(index) {
+      const infoDiv = document.getElementById("info");
+      if (index >= 0 && index < descripciones.length)
+    {infoDiv.textContent = descripciones[index];infoDiv.classList.remove("hidden");} 
+      else { console.error("Imagen fuera de rango");}}
